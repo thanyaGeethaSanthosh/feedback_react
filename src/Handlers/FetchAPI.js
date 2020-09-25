ce1fcf4dad1a57b2e19cea53bd6f8d458968ba56
thanya <32271882+ThanyaSanthosh@users.noreply.github.com>
2020-09-25 14:56:03 +0530

    feedbacks fetching given to backend

--- src/Handlers/FetchAPI.js
+++ src/Handlers/FetchAPI.js
@@ -19,68 +19,22 @@ const Register = (data) => {
 
 const getOtherUserData = (userName) => {
   return fetch(`/api/user/${userName}`).then((res) => res.json());
-  // .then(({ otherUser }) => otherUser);
 };
 
 const getUserData = () => {
   return fetch('/api/getUserData').then((res) => res.json());
 };
 
-const FeedBacks = [
-  {
-    nameToShow: 'Shashi kumar',
-    recipient: 'ram',
-    relatedTo: 'Teachers day presentation',
-    sender: 'thanya',
-    suggestion: 'your presentation was awsome',
-    time: '2020-09-24T06:20:46.266Z',
-  },
-  {
-    nameToShow: 'Anonymous',
-    recipient: 'thanya',
-    relatedTo: 'new year Party',
-    sender: 'micheal',
-    suggestion: 'next time give invitation little earlier',
-    time: '2020-01-2T06:20:46.266Z',
-  },
-];
-
 const addFeedBack = (data) => {
-  getUserData()
-    .then(({ user }) => user)
-    .then(({ userID }) => {
-      FeedBacks.push({
-        ...data,
-        sender: userID,
-        time: new Date(),
-        id: FeedBacks.length,
-      });
-    });
-  return new Promise((resolve) => resolve('ok'));
+  return postReq('/api/addFeedBack', data);
 };
 
-const getSentFeedBacks = () => {
-  return new Promise((resolve) => {
-    getUserData()
-      .then(({ user }) => user)
-      .then(({ userID }) => {
-        const sent = FeedBacks.filter((feedBack) => feedBack.sender === userID);
-        resolve(sent);
-      });
-  });
+const getSentFeedBacks = (data) => {
+  return fetch('/api/getSentFeedBacks').then((res) => res.json());
 };
 
-const getReceivedFeedBacks = () => {
-  return new Promise((resolve) => {
-    getUserData()
-      .then(({ user }) => user)
-      .then(({ userID }) => {
-        const sent = FeedBacks.filter(
-          (feedBack) => feedBack.recipient === userID
-        );
-        resolve(sent);
-      });
-  });
+const getReceivedFeedBacks = (data) => {
+  return fetch('/api/getReceivedFeedBacks').then((res) => res.json());
 };
 
 export default {
